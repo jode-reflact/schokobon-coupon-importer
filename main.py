f08dc682e551c18a9c9368eb08e721872f744c0d
Jonas Deterding <jonas.deterding@reflact.com>
2024-01-25 20:37:01 +0100

    🔖 (V1): Finished coupon collection

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import argparse
+import os
 import time
 
+import pandas as pd
 from selenium.webdriver import Firefox
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.expected_conditions import \
-    element_to_be_clickable
 
 
 class CollectCoupons:
     def __init__(self, code_file_path, email) -> None:
+        df_path = 'code_status.xlsx'
+        
+        if os.path.exists(df_path):
+            df = pd.read_excel(df_path)
+        else:
+            df = pd.DataFrame(columns=['Code', 'Used', 'Downloaded'])
+
         with open(code_file_path, 'r') as file:
             codes = file.readlines()
 
@@ -17,17 +24,26 @@ class CollectCoupons:
         #opts.add_argument("--headless")
         browser = Firefox(options=opts)
         browser.implicitly_wait(2)
-        for code in [codes[12]]: #codes:
+        for code in codes:
             code = code.strip()
-            browser.get('https://www.kinder.com/de/de/xp/suchspass/#teilnehmen')
+            #Check if code is already in df
+            if len(df) > 0 and code in df['Code'].values:
+                continue
 
-            cookies = browser.find_element(By.CSS_SELECTOR, '.ot-bnr-save-handler')
-            if cookies is not None:
-                cookies.click()
-            #class ot-bnr-save-handler
-                #.ot-bnr-save-handler
-            #id=code
-            #id=email
+            browser.refresh()
+            browser.get('https://www.kinder.com/de/de/xp/suchspass/#teilnehmen')
+            time.sleep(1)
+
+            #Accept some cookies if needed
+            try:
+                cookies = browser.find_element(By.CSS_SELECTOR, '.ot-bnr-save-handler')
+                if cookies.is_displayed():
+                    cookies.click()
+            except Exception as e:
+                # Cookies already accepted
+                cookies = None
+
+            # Fill form and submit
             code_input = browser.find_element(By.ID, 'code')
             code_input.send_keys(code)
 
@@ -38,30 +54,50 @@ class CollectCoupons:
 
             time.sleep(2)
 
+            # Checks if used was already used
             try:
                 code_already_used = browser.find_element(By.CSS_SELECTOR, '[x-show="usedCode"]')
                 if code_already_used.is_displayed():
-                    print("code already used")
+                    print(f"Code {code} already used")
+                    df.loc[len(df)] = [code, True, False] # Code, Used, Downloaded
+                    df.to_excel(df_path, index=False)
                     continue
-            except:
-                print("code not used")
+            except Exception as e:
+                # Code not used
+                code_already_used = None
 
+            #Find "switch-2" which is the element with the correct answer
             switch = browser.find_element(By.ID, 'switch-2')
-            #ActionChains(browser).scroll_to_element(switch).click(switch).perform() #fix this
+
+            #Scroll into view to enable click
             browser.execute_script('arguments[0].scrollIntoView({ block: "center", inline: "center" });', switch)
-            #element_to_be_clickable(switch)
             time.sleep(2)
+
+            #Click and Submit answer
             switch.click()
             switch.submit()
-            time.sleep(10)
-            #id switch-2
-            #class="relative inline-block min-w-[240px] min-h-[80px] group focus:outline-0 disabled:hover:none"
+
+            #Saving used code to dataframe
+            df.loc[len(df)] = [code, True, False] # Code, Used, Downloaded
+            df.to_excel(df_path, index=False)
+
+            #Cooldown time
+            time.sleep(5)
+
+        browser.quit()
 
 class SaveCouponPdfs():
     def __init__(self) -> None:
-        #empty
+        #future work
         pass
 
 if __name__ == '__main__':
-    CollectCoupons(code_file_path='codes.txt', email='sjonbon@jdeterding.de')
+    parser = argparse.ArgumentParser(
+                    prog='Schokobon-Coupon-Collector')
+    parser.add_argument('-f', '--file_path', type=str, required=True)
+    parser.add_argument('-e', '--email', type=str, required=True)
+
+    args = parser.parse_args()
+
+    CollectCoupons(code_file_path=args.file_path, email=args.email)
     SaveCouponPdfs()
